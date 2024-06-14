729d694d5bee229662a3878bb9680daa0cc53b4b
Fasika Dagnachew <fasika1515@gmail.com>
2024-06-14 23:13:30 +0300

    hero img

--- app/contact/Hero.tsx
+++ app/contact/Hero.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='relative w-full h-[650px] flex items-center justify-center mb-40'>
+    <div className='relative w-full max-h-[450px] h-[650px] flex items-center justify-center mb-40'>
         <Image className='relative w-full h-full object-cover' src='/images/Contact.png' width={1920} height={650} alt='Hero' quality={100} />
         <h2 className='absolute text-5xl sm:text-7xl text-white font-medium'>Contact Us</h2>
     </div>
